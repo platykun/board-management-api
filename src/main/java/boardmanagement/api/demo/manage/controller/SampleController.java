223f47d2f5e7b5317503b1f3be181d4f7fa48905
platykun <nagonago561@yahoo.co.jp>
2018-05-04 17:46:34 +0900

    add cross origin

--- src/main/java/boardmanagement/api/demo/manage/controller/SampleController.java
+++ src/main/java/boardmanagement/api/demo/manage/controller/SampleController.java
@@ -1,8 +1,10 @@
 package boardmanagement.api.demo.manage.controller;
 
 import boardmanagement.api.demo.manage.service.SampleService;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * サンプル実装
  */
 @RestController
+@CrossOrigin
 public class SampleController {
 
     @Autowired
@@ -23,10 +26,17 @@ public class SampleController {
      * @return トップ画面
      */
     @RequestMapping("")
-    public String top(Model model) {
+    public SampleResult top(Model model) {
 
         sampleService.callRepositorySample();
-        return "hogehoge";
+
+        return new SampleResult();
+    }
+
+    @Data
+    class SampleResult {
+        private String name = "sample";
+        private String password = "p@ssw0rd";
     }
 }
 
